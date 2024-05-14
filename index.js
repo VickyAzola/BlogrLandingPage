12b8ff856cb5b219eab2c7d4c070cfba1f2d604d
Victoria Azola Silva <116470398+VickyAzola@users.noreply.github.com>
2024-05-13 23:41:14 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const nav = document.getElementById("topMenu");
+const navButtonIcon = document.getElementById("navButtonIcon");
 const button = document.getElementById("buttonGroup");
+const dropdownBtn = document.querySelectorAll(".dropdownBtn");
+const arrowIcon = document.querySelectorAll(".arrowIcon");
+
 
 function showNavMenu() {
-    if (nav.className === "topMenu") {
-        nav.className += " responsive";
-        button.className += " responsive";
+    nav.classList.toggle("hidden")
+
+    if (nav.classList.contains("hidden")) {
+        navButtonIcon.src = "../images/icon-hamburger.svg"
     } else {
-        nav.className = "topMenu";
-        button.className = "buttonGroup";
+        navButtonIcon.src = "../images/icon-close.svg"
     }
 }
 
-document.addEventListener("touchstart", function() {}, true);
+for (let i = 0; i < dropdownBtn.length; i++) {
+    dropdownBtn[i].addEventListener("click", function() {
+        let content = this.nextElementSibling;
+        content.classList.toggle("hidden");
+
+        arrowIcon[i].style.transform = content.classList.contains("hidden") ? "rotate(0deg)" : "rotate(180deg)";
+    });
+}
